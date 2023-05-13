d3cc0a2e9ea69abf6dbd23ffeff81f6838d15cff
Perchant76 <63096336+Perchant76@users.noreply.github.com>
2023-05-13 19:28:06 +0200

    Update script.sh

--- server/script.sh
+++ server/script.sh
@@ -1,5 +1,5 @@
 curl -fsSL https://starship.rs/install.sh | bash
-sudo apt install fish makepkg
+sudo apt install fish makepkg bat
 mkdir -p ~/.config && touch ~/.config/starship.toml
 sudo cp -r fish/config.fish /etc/config/fish/config.fish
 sudo cp -r starship/starship.toml ~/.config/starfish.toml
