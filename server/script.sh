8f851e31dda1591b82dec0a8ed30c7a530643ebb
Perchant76 <63096336+Perchant76@users.noreply.github.com>
2023-05-13 19:18:00 +0200

    Update script.sh

--- server/script.sh
+++ server/script.sh
@@ -1,6 +1,11 @@
 curl -fsSL https://starship.rs/install.sh | bash
-sudo apt install fish
+sudo apt install fish makepkg
 mkdir -p ~/.config && touch ~/.config/starship.toml
 sudo cp -r fish/config.fish /etc/config/fish/config.fish
 sudo cp -r starship/starship.toml ~/.config/starfish.toml
 sudo chsh -s /bin/fish
+cd 
+git clone https://gitlab.com/dwt1/shell-color-scripts.git
+cd shell-color-scripts
+makepkg -cf
+sudo make install
